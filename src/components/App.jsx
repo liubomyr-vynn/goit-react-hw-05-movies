b51a7bf95c76142765f33d7b69ff3f0c2ac0e2f8
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-05-28 23:56:23 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
-export const App = () => {
+import { Route, Routes } from 'react-router-dom';
+
+import Home from 'pages/Home';
+import Movies from 'pages/Movies';
+import MovieDetails from 'pages/MovieDetails';
+import Layout from './Layout';
+import CastInformation from './CastInformation';
+import ReviewsInformation from './ReviewsInformation';
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<CastInformation />} />
+          <Route path="reviews" element={<ReviewsInformation />} />
+        </Route>
+      </Route>
+    </Routes>
   );
 };
+
+export default App;
