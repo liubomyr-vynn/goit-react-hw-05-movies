6b89d6b54229e915499408c2aa4f9e95641d9409
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-05-28 23:56:40 +0300

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import { Link, useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+const Home = () => {
+  const [trending, setTrending] = useState([]);
+  const location = useLocation();
+
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      url: 'https://api.themoviedb.org/3/trending/movie/day',
+      params: { language: 'en-US' },
+      headers: {
+        accept: 'application/json',
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMmQ0ODE5YjFkMzFjMGU5YzVmY2Q0YWUxN2NlYzBiMiIsInN1YiI6IjY0NzA2NjEwNzcwNzAwMDEzNjdlZmY4YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BH5Ex4koF22N6gg0mj72NBJQ7EPqMZd2hU1unPYNLqA',
+      },
+    };
+    axios
+      .request(options)
+      .then(function (response) {
+        setTrending(response.data.results);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <ul>
+      {trending.map(dataItem => {
+        return (
+          <li key={dataItem.id}>
+            <Link to={`/movies/${dataItem.id}`} state={{ from: location }}>
+              {dataItem.title}
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+export default Home;
