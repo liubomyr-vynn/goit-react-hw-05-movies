5aa1d0b64fee53134267010a59b47e5a17bd6da2
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-05-28 23:57:07 +0300

    Create Layout.jsx

---
+++ src/components/Layout.jsx
@@ -0,0 +1,32 @@
+import { NavLink, Outlet } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledLink = styled(NavLink)`
+  color: black;
+
+  &.active {
+    color: red;
+  }
+`;
+
+const Layout = () => {
+  return (
+    <div>
+      <header>
+        <ul>
+          <li>
+            <StyledLink to="/">Home</StyledLink>
+          </li>
+          <li>
+            <StyledLink to="/movies">Movies</StyledLink>
+          </li>
+        </ul>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <footer>Footer</footer>
+    </div>
+  );
+};
+export default Layout;
