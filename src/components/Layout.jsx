df8799267a9470854ce61e7693b2e04835a0b7d3
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-06-02 00:06:08 +0300

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -23,9 +24,10 @@ const Layout = () => {
         </ul>
       </header>
       <main>
-        <Outlet />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Outlet />
+        </Suspense>
       </main>
-      <footer>Footer</footer>
     </div>
   );
 };
