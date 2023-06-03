ce3a686abc1406e66997c6fc01cbe2839575a42d
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-06-04 01:02:11 +0300

    Update Home.styled.jsx

--- src/pages/Home/Home.styled.jsx
+++ src/pages/Home/Home.styled.jsx
@@ -1,14 +1,17 @@
 import styled from 'styled-components';
 
 export const HomeContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  /* display: flex; */
+  /* align-items: center;
+  justify-content: center; */
+`;
+
+export const HomeTitle = styled.h2`
+  margin-bottom: 20px;
 `;
 
 export const HomeList = styled.ul`
   display: flex;
   flex-direction: column;
-
   gap: 8px;
 `;
