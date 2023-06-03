671977412426e27d4b2881e7d4a05ea3f31f7c4a
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-06-04 01:02:05 +0300

    Update MovieDetails.styled.jsx

--- src/pages/MovieDetails/MovieDetails.styled.jsx
+++ src/pages/MovieDetails/MovieDetails.styled.jsx
@@ -3,19 +3,38 @@ import styled from 'styled-components';
 export const MovieDetailsContainer = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  /* align-items: center;
+  justify-content: center; */
   gap: 20px;
   margin-bottom: 20px;
 `;
 
 export const BackLinkInfo = styled.div`
-  display: flex;
   margin-right: auto;
+  display: inline-block;
+
+  outline: none;
+  cursor: pointer;
+  font-weight: 600;
+  border-radius: 3px;
+  padding: 8px 20px;
+  border: 1px solid #42ecc2;
+  line-height: 1.15;
+  font-size: 16px;
+  margin-left: 15px;
+
+  &:hover {
+    transition: all 0.1s ease;
+    box-shadow: 0 0 0 0 #fff, 0 0 0 3px #1de9b6;
+  }
 `;
 
 export const MainInfo = styled.div`
   display: flex;
+  @media screen and (max-width: 700px) {
+    flex-direction: column;
+    gap: 20px;
+  }
 `;
 
 export const MainInfoContainer = styled.div`
@@ -23,6 +42,9 @@ export const MainInfoContainer = styled.div`
   flex-direction: column;
   gap: 20px;
   margin-left: 30px;
+  @media screen and (max-width: 700px) {
+    margin-left: 0px;
+  }
 `;
 
 export const AdditionalInfo = styled.div`
@@ -31,7 +53,6 @@ export const AdditionalInfo = styled.div`
   gap: 20px;
   align-items: center;
   justify-content: center;
-  // margin-bottom: 20px;
 `;
 
 export const AdditionalList = styled.ul`
