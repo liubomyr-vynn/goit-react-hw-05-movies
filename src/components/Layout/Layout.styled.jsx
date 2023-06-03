de1175e6971e8eb141dbef688552e520323721f8
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-06-04 01:02:19 +0300

    Update Layout.styled.jsx

--- src/components/Layout/Layout.styled.jsx
+++ src/components/Layout/Layout.styled.jsx
@@ -6,21 +6,34 @@ export const Container = styled.div`
 `;
 
 export const Header = styled.header`
-  display: flex;
-  margin-bottom: 20px;
-  align-items: center;
-  justify-content: center;
-
-  padding-top: 20px;
+  /* padding-top: 20px;
   padding-bottom: 20px;
-  background-color: gray;
+  background-color: #b4b4b4;
+  margin-bottom: 20px; */
+  top: 0;
+  left: 0;
+  position: sticky;
+  z-index: 1100;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 40px;
+  padding-right: 24px;
+  padding-left: 24px;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  color: #fff;
+  background-color: #858586ea;
+  box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
+    0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
+  margin-bottom: 20px;
 `;
 
 export const HeaderList = styled.ul`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 8px;
+  cursor: pointer;
+  font-weight: 600;
+  font-size: 18px;
+  margin-left: 40px;
 `;
 
 export const HeaderItem = styled.li`
@@ -28,11 +41,16 @@ export const HeaderItem = styled.li`
   width: 70px;
   color: white;
   text-align: center;
+  border-radius: 3px;
   padding: 10px 0;
   &:active {
-    background-color: #121212;
+    background-color: #995618;
   }
   &:hover {
-    background-color: #333333;
+    background-color: #8b96f7;
   }
 `;
+
+// &:active {
+//   border-bottom: 1px solid #50ec42;
+// }
