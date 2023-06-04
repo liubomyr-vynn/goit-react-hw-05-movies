9feda38ae7462a97e2b40553c851ed5a386c0b97
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-06-04 17:22:12 +0300

    Update Cast.jsx

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -38,28 +38,6 @@ const Cast = () => {
     FetchCast();
   }, [params.movieId]);
 
-  // useEffect(() => {
-  //   const options = {
-  //     method: 'GET',
-  //     url: `https://api.themoviedb.org/3/movie/${params.movieId}/credits`,
-  // params: { language: 'en-US' },
-  // headers: {
-  //   accept: 'application/json',
-  //   Authorization:
-  //     'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMmQ0ODE5YjFkMzFjMGU5YzVmY2Q0YWUxN2NlYzBiMiIsInN1YiI6IjY0NzA2NjEwNzcwNzAwMDEzNjdlZmY4YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BH5Ex4koF22N6gg0mj72NBJQ7EPqMZd2hU1unPYNLqA',
-  // },
-  //   };
-
-  //   axios
-  //     .request(options)
-  //     .then(function (response) {
-  //       setCastList(response.data.cast);
-  //     })
-  //     .catch(function (error) {
-  //       console.error(error);
-  //     });
-  // }, [params.movieId]);
-
   return (
     <div>
       {loading ? (
