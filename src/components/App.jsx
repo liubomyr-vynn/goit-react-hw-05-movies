bf2f14f1d18af43fac6f044f7e85b479fb6b7fe0
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-06-04 17:21:58 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ const Home = lazy(() => import('../pages/Home/Home'));
 const Movies = lazy(() => import('../pages/Movies/Movies'));
 const MovieDetails = lazy(() => import('../pages/MovieDetails/MovieDetails'));
 const Cast = lazy(() => import('./Cast/Cast'));
-const Reviews = lazy(() => import('./Reviews'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
 
 const App = () => {
   return (
