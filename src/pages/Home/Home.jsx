7d51e339233380df736e4c111e83607d31bb6cc1
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-06-04 17:22:47 +0300

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,48 +1,67 @@
 import axios from 'axios';
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
-import { HomeContainer, HomeList, HomeTitle } from './Home.styled';
+import {
+  HomeContainer,
+  HomeList,
+  HomeTitle,
+  ItemLink,
+  ListItem,
+} from './Home.styled';
+import Loader from '../../components/Loader/Loader';
 
 const Home = () => {
   const [trending, setTrending] = useState([]);
+  const [loading, setLoading] = useState(false);
   const location = useLocation();
 
   useEffect(() => {
-    const options = {
-      method: 'GET',
-      url: 'https://api.themoviedb.org/3/trending/movie/day',
-      params: { language: 'en-US' },
-      headers: {
-        accept: 'application/json',
-        Authorization:
-          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMmQ0ODE5YjFkMzFjMGU5YzVmY2Q0YWUxN2NlYzBiMiIsInN1YiI6IjY0NzA2NjEwNzcwNzAwMDEzNjdlZmY4YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BH5Ex4koF22N6gg0mj72NBJQ7EPqMZd2hU1unPYNLqA',
-      },
-    };
-    axios
-      .request(options)
-      .then(function (response) {
+    setLoading(true);
+    const FetchHome = async () => {
+      try {
+        const response = await axios.get(
+          'https://api.themoviedb.org/3/trending/movie/day',
+          {
+            params: { language: 'en-US' },
+            headers: {
+              accept: 'application/json',
+              Authorization:
+                'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMmQ0ODE5YjFkMzFjMGU5YzVmY2Q0YWUxN2NlYzBiMiIsInN1YiI6IjY0NzA2NjEwNzcwNzAwMDEzNjdlZmY4YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BH5Ex4koF22N6gg0mj72NBJQ7EPqMZd2hU1unPYNLqA',
+            },
+          }
+        );
         setTrending(response.data.results);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      }
+    };
+    FetchHome();
   }, []);
 
   return (
     <HomeContainer>
       <HomeTitle>Trending movies:</HomeTitle>
-      <HomeList>
-        {trending.map(dataItem => {
-          return (
-            <li key={dataItem.id}>
-              <Link to={`/movies/${dataItem.id}`} state={{ from: location }}>
-                {dataItem.title}
-              </Link>
-            </li>
-          );
-        })}
-      </HomeList>
+      {loading ? (
+        <Loader />
+      ) : (
+        <HomeList>
+          {trending.map(dataItem => {
+            return (
+              <ListItem key={dataItem.id}>
+                <ItemLink
+                  to={`/movies/${dataItem.id}`}
+                  state={{ from: location }}
+                >
+                  {dataItem.title}
+                </ItemLink>
+              </ListItem>
+            );
+          })}
+        </HomeList>
+      )}
     </HomeContainer>
   );
 };
